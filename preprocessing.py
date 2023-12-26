7ec6448747b8a9504a571cbc7dc179d634b8c17c
paabram <patrick.w.aabram@gmail.com>
2023-12-26 14:02:54 -0500

    finished preprocessor and wrapped it in a function

--- preprocessing.py
+++ preprocessing.py
@@ -1,60 +1,53 @@
 import pandas as pd
-from sklearn.cluster import KMeans
 
-# create dataframe
-colors = pd.read_csv('color_names.csv')
-# drop columns that won't be used
-colors.drop(columns=['Hex (24 bit)', 'Hue (degrees)', 'HSL.S (%)','HSL.L (%), HSV.S (%), HSV.V (%)'], inplace=True)
-# rename relevant columns for usability
-colors.rename(columns={'Red (8 bit)': 'Red', 'Green (8 bit)': 'Green', 'Blue (8 bit)':'Blue'}, inplace=True)
+def data():
+    # create dataframe
+    colors = pd.read_csv('color_names.csv')
+    # drop columns that won't be used
+    colors.drop(columns=['Hex (24 bit)', 'Hue (degrees)', 'HSL.S (%)','HSL.L (%), HSV.S (%), HSV.V (%)'], inplace=True)
+    # rename relevant columns for usability
+    colors.rename(columns={'Red (8 bit)': 'Red', 'Green (8 bit)': 'Green', 'Blue (8 bit)':'Blue'}, inplace=True)
 
-# reassign names to limited pool of simple color terms
-categories = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'pink', 'brown', 'white', 'gray', 'black']
-bad_indices = []
-# 
-for i in range(len(colors)):
-    name = colors.Name.iloc[i]
-    if 'purple' in name:
-        name = 'violet'
-    if 'grey' in name:
-        name = 'gray'
-    flag = 0
-    for c in categories:
-        if c in name:
-            colors.at[i, 'Name'] = c
-            flag = 1
-            break
-    if flag == 0:
-        bad_indices.append(i)
+    # reassign names to limited pool of simple color terms
+    categories = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'pink', 'brown', 'white', 'gray', 'black']
+    bad_indices = []
+    
+    # iterate through the dataframe
+    for i in range(len(colors)):
+        name = colors.Name.iloc[i] # pull the color name
+        
+        # reconcile the variation in naming in the dataset
+        if 'purple' in name:
+            name = 'violet'
+        elif 'grey' in name:
+            name = 'gray'
+        elif 'magenta' in name or 'rose' in name:
+            name = 'pink'
+        elif 'gold' in name:
+            name = 'yellow'
+        elif 'beige' in name or 'tan' in name:
+            name = 'brown'
 
-colors = colors.drop(index=bad_indices)
-colors.reset_index(inplace=True)
+        # check if any of the simpler color terms appear in the color's name
+        flag = 0
+        for c in categories:
+            if c in name:
+                # rename the color
+                colors.at[i, 'Name'] = c
+                flag = 1
+                # no break statement here, based on the intuition that the last term in a color's name is typically the dominant hue
+        
+        # if no match was found add this index to an array of indices to drop later
+        if flag == 0:
+            bad_indices.append(i)
 
-print(colors)
+    # drop bad indices and reset index
+    colors.drop(index=bad_indices, inplace=True)
+    colors.reset_index(drop=True, inplace=True)
 
-# color data will be clustered into simpler, basic color terms
-# 11 clusters: red, orange, yellow, green, blue, violet, pink, brown, white, gray, black
-# centers = [[255, 0, 0], [255, 127, 0], [255, 255, 0], [0, 255, 0], [0, 0, 255], [165, 0, 255], \
-#            [255, 0, 255], [127, 63, 0], [255, 255, 255], [127, 127, 127], [0, 0, 0]]
-# # centers = [[218, 34, 79], [228, 125, 44], [255, 255, 0], [0, 255, 0], [0, 0, 255], [165, 0, 255], \
-#         #    [255, 0, 255], [127, 63, 0], [255, 255, 255], [127, 127, 127], [0, 0, 0]]
-# namer = KMeans(n_clusters=11, n_init=1, init=centers, random_state=255)
-# namer.fit(X)
-# # add these as a column in the dataframe
-# colors['Name'] = namer.labels_
+    return colors
 
-# # rename to corresponding terms
-# def name_colors(n):
-#     colors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'pink', 'brown', 'white', 'gray', 'black']
-#     return colors[n]
-
-# colors.Name = colors.Name.apply(name_colors)
-
-# # this code was used to check what color each of these labels corresponds to for manual naming
-# # min_rgb = colors.groupby(['Name']).min()
-# # mean_rgb = colors.groupby(['Name']).mean()
-# # max_rgb = colors.groupby(['Name']).max()
-
-# import random
-# print(colors.iloc[random.randint(0, 1297)])
-# 0=red, 1=orange, 2=yellow, 3=green, 4=blue, 5=violet, 6=pink, 7, 
+# this code was used to check accuracy of naming
+# if __name__ == '__main__':
+#     mean_rgb = data().groupby(['Name']).mean()
+#     print(mean_rgb)
